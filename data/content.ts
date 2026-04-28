b6eb43891f94855ac0b1031a3c2833a677fcc666
carloscarvalho <misterchaty@outlook.com>
2026-04-28 17:27:19 -0300

    12

---
+++ data/content.ts
@@ -0,0 +1,8 @@
+export const campaigns = {
+    "botox": {
+        title: "Title Of My First Campaign",
+        subtitle: "Yes, you can do it my litle bro! lotem ipsum hahahaha",
+        mainButton: "Agende Agora!",
+        secondaryBytton: "Conheça Nossos Métodos"
+    }
+}
